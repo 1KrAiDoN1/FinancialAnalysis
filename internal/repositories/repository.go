bc8a00e3e5e0640dfda5e93f406443b44eced244
1KrAiDoN1 <pavelvasilev24843@gmail.com>
2025-06-05 15:03:33 +0300

    repo

---
+++ internal/repositories/repository.go
@@ -0,0 +1,20 @@
+package repositories
+
+// "finance/internal/models"
+
+// "github.com/jackc/pgx/v5/pgxpool"
+
+// type Authorization interface {
+// 	CreateUser(user models.User) (int, error)
+// 	GetUser(username, password string) (models.User, error)
+// }
+
+// type Repository struct {
+// 	Authorization
+// }
+
+// func NewRepository(pool *pgxpool.Pool) *Repository {
+// 	return &Repository{
+// 		Authorization: NewAuthRepository(pool),
+// 	}
+// }
