5bdcec67e029484eab3a2923b785278f0893e23d
1KrAiDoN1 <pavelvasilev24843@gmail.com>
2025-06-05 15:04:16 +0300

    service

---
+++ internal/services/service.go
@@ -0,0 +1,20 @@
+package services
+
+// type Service struct { // создаем структуру, которая будет содержать интерфейсы
+// 	AuthService
+// 	ExpenseService
+// 	CategoryService
+// 	UserService
+// 	BudgetService
+// }
+
+// func NewService(repo *repositories.Repository) *Service {
+// 	return &Service{
+// 		AuthService:     NewAuthService(repo),
+// 		ExpenseService:  NewExpenseService(repo),
+// 		CategoryService: NewCategoryService(repo),
+// 		UserService:     NewUserService(repo),
+// 		BudgetService:   NewBudgetService(repo),
+// 	}
+
+// }
