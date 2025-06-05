412847c91843a3a4af238bb9840c0338159aaa00
1KrAiDoN1 <pavelvasilev24843@gmail.com>
2025-06-05 14:59:26 +0300

    container of structures

---
+++ internal/container/contaner.go
@@ -0,0 +1,95 @@
+package container
+
+import (
+	"context"
+	//"finance/internal/config"
+	"finance/internal/database"
+	"finance/internal/handler"
+	"finance/internal/repositories"
+	"finance/internal/services"
+)
+
+type Container struct {
+	//Config *config.Config
+	DB *database.Database
+
+	// Repositories
+	AuthRepo     *repositories.AuthRepository
+	UserRepo     *repositories.UserRepository
+	CategoryRepo *repositories.CategoryRepository
+	ExpenseRepo  *repositories.ExpenseRepository
+	BudgetRepo   *repositories.BudgetRepository
+
+	// Services
+	AuthService     *services.AuthService
+	UserService     *services.UserService
+	CategoryService *services.CategoryService
+	ExpenseService  *services.ExpenseService
+	BudgetService   *services.BudgetService
+
+	// Handlers
+	AuthHandler     *handler.AuthHandler
+	UserHandler     *handler.UserHandler
+	CategoryHandler *handler.CategoryHandler
+	ExpenseHandler  *handler.ExpenseHandler
+	BudgetHandler   *handler.BudgetHandler
+}
+
+func NewContainer() (*Container, error) {
+	ctx := context.Background()
+	// Load config
+	databaseURL, err := database.NewDatabaseURL()
+	if err != nil {
+		return nil, err
+	}
+
+	// Initialize database
+	DB, err := database.NewDatabase(ctx, databaseURL)
+	if err != nil {
+		return nil, err
+	}
+	dbpool := DB.GetPool()
+
+	// Initialize repositories
+	authRepo := repositories.NewAuthRepository(dbpool)
+	userRepo := repositories.NewUserRepository(dbpool)
+	categoryRepo := repositories.NewCategoryRepository(dbpool)
+	expenseRepo := repositories.NewExpenseRepository(dbpool)
+	budgetRepo := repositories.NewBudgetRepository(dbpool)
+
+	// Initialize services
+	authService := services.NewAuthService(authRepo)
+	userService := services.NewUserService(userRepo)
+	categoryService := services.NewCategoryService(categoryRepo)
+	expenseService := services.NewExpenseService(expenseRepo, categoryRepo)
+	budgetService := services.NewBudgetService(budgetRepo, expenseRepo)
+
+	// Initialize handlers
+	authHandler := handler.NewAuthHandler(authService)
+	userHandler := handler.NewUserHandler(userService)
+	categoryHandler := handler.NewCategoryHandler(categoryService)
+	expenseHandler := handler.NewExpenseHandler(expenseService)
+	budgetHandler := handler.NewBudgetHandler(budgetService)
+
+	return &Container{
+		//Config: cfg,
+		DB:           DB,
+		AuthRepo:     authRepo,
+		UserRepo:     userRepo,
+		CategoryRepo: categoryRepo,
+		ExpenseRepo:  expenseRepo,
+		BudgetRepo:   budgetRepo,
+
+		AuthService:     authService,
+		UserService:     userService,
+		CategoryService: categoryService,
+		ExpenseService:  expenseService,
+		BudgetService:   budgetService,
+
+		AuthHandler:     authHandler,
+		UserHandler:     userHandler,
+		CategoryHandler: categoryHandler,
+		ExpenseHandler:  expenseHandler,
+		BudgetHandler:   budgetHandler,
+	}, nil
+}
