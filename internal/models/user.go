3695c9ad18b1aa4b45ba5e26f35536602602a544
1KrAiDoN1 <pavelvasilev24843@gmail.com>
2025-06-20 00:02:02 +0300

    changed struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,8 +3,9 @@ package models
 import "time"
 
 type User struct {
-	ID                 string    `json:"id"`
-	Username           string    `json:"username" binding:"required"`
+	ID                 int       `json:"id"`
+	FirstName          string    `json:"first_name" validate:"required,min=2,max=50" example:"John"`
+	LastName           string    `json:"last_name" validate:"required,min=2,max=50" example:"Doe"`
 	Email              string    `json:"email" binding:"required"`
 	Password           string    `json:"password" binding:"required"`
 	TimeOfRegistration time.Time `json:"time_of_registration"`
