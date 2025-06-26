91104c296c495ae58402c7003d169a38b42ee65f
1KrAiDoN1 <pavelvasilev24843@gmail.com>
2025-06-26 19:23:34 +0300

    delete comments

--- internal/http-server/server.go
+++ internal/http-server/server.go
@@ -9,18 +9,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// type Server struct {
-// 	httpServer *http.Server
-// }
-
-// func (s *Server) Run(port string, handler http.Handler) error {
-// 	s.httpServer = &http.Server{
-// 		Addr:    port,
-// 		Handler: handler,
-// 	}
-// 	return s.httpServer.ListenAndServe()
-// }
-
 // func (s *Server) Shutdown(ctx context.Context) error {
 // 	return s.httpServer.Shutdown(ctx)
 // }
