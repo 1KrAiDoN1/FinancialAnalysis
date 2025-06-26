8eb7e1a15bd84e38294cd3afa6bebce8db1e34e1
1KrAiDoN1 <pavelvasilev24843@gmail.com>
2025-06-26 19:24:30 +0300

    fix

--- internal/storages/database/database.go
+++ internal/storages/database/database.go
@@ -27,7 +27,8 @@ func NewDatabase(ctx context.Context, databaseURL string) (*Storage, error) {
 		return nil, fmt.Errorf("failed to ping database: %w", err)
 	}
 	return &Storage{
-		pool: pool}, nil
+			pool: pool},
+		nil
 
 }
 
